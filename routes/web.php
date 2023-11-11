ec4d7802f21f7d7c0387208918e4b233eff998f4
Nathan <75358980+nathanaelabel@users.noreply.github.com>
2023-11-11 22:02:22 +0700

    fix: add missing vehicle controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\VehicleController;
 use App\Http\Controllers\CarController;
 use App\Http\Controllers\MotorcycleController;
 use App\Http\Controllers\TruckController;
