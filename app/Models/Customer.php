a833ec1fa351be73f4e50600b851002ee5036c1a
Nathanael Abel <75358980+nathanaelabel@users.noreply.github.com>
2023-11-12 04:44:32 +0700

    fix: models adjustment

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -10,7 +10,7 @@ class Customer extends Model
     protected $fillable = ['name', 'address', 'phone_number', 'id_card'];
 
     // Define relationships
-    public function orders()
+    public function order()
     {
         return $this->hasMany(Order::class);
     }
